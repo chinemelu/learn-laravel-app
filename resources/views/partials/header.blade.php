1b9cc6f3a6ad7b8b42b6223ae6ad7d7e43bee7c9
Chinemelu Nwosu <chinemelunwosu@gmail.com>
2021-02-08 23:58:36 +0100

    create partials and layout

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Brand</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0 me-5">
+        <li class="nav-item">
+          <a class="nav-link" href="#"><i class="fa fa-shopping-cart"></i> Shopping Cart</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <i class="fa fa-user"></i> User Account
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">User Account</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Logout</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -5,5 +5,13 @@
 @endsection
 
 @section('content')
-  <h1>It works!!</h1>
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Button</a>
+    <a href="#" class="btn btn-outline-secondary">Button</a>
+  </div>
+</div>
 @endsection

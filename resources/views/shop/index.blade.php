35cda09c5c9d40552aafa925899037f0938cecac
Chinemelu Nwosu <chinemelunwosu@gmail.com>
2021-02-15 22:24:33 +0100

    get data from database

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -5,84 +5,23 @@
 @endsection
 
 @section('content')
-  <div class="row">
-    <div class="col-md-4 col-sm-6">
-      <div class="card" style="width: 18rem;">
-        <img src="{{ URL::to('images/harry-potter-and-cos.jpg') }}" class="card-img-top img-responsive" alt="Harry Potter Book Poster">
-        <div class="card-body">
-          <h5 class="card-title">Product Title</h5>
-          <p class="card-text description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem corrupti expedita architecto sapiente error quibusdam illum facilis temporibus. Sit, expedita. Dicta excepturi earum et accusamus, quaerat tempora commodi unde itaque.</p>
-          <div class="pull-left price">$12</div>
-          <div class="add-to-cart-container">
-            <a href="#" class="btn btn-success pull-right">Add to Cart</a>
-          </div>
-        </div> 
+  @foreach($products->chunk(3) as $productChunk)
+   <div class="row">
+    @foreach($productChunk as $product)
+      <div class="col-md-4 col-sm-6">
+        <div class="card">
+          <img src="{{ $product->imagePath }}" class="card-img-top img-responsive" alt="Harry Potter Book Poster">
+          <div class="card-body">
+            <h5 class="card-title">{{ $product->title }}</h5>
+            <p class="card-text description"></p>
+            <div class="pull-left price">{{ $product->price }}</div>
+            <div class="add-to-cart-container">
+              <a href="#" class="btn btn-success pull-right">Add to Cart</a>
+            </div>
+          </div> 
+        </div>
       </div>
-    </div>
-    <div class="col-md-4 col-sm-6">
-      <div class="card" style="width: 18rem;">
-        <img src="{{ URL::to('images/harry-potter-and-cos.jpg') }}" class="card-img-top img-responsive" alt="Harry Potter Book Poster">
-        <div class="card-body">
-          <h5 class="card-title">Product Title</h5>
-          <p class="card-text description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem corrupti expedita architecto sapiente error quibusdam illum facilis temporibus. Sit, expedita. Dicta excepturi earum et accusamus, quaerat tempora commodi unde itaque.</p>
-          <div class="pull-left price">$12</div>
-          <div class="add-to-cart-container">
-            <a href="#" class="btn btn-success pull-right">Add to Cart</a>
-          </div>
-        </div> 
-      </div>
-    </div>
-    <div class="col-md-4 col-sm-6">
-      <div class="card" style="width: 18rem;">
-        <img src="{{ URL::to('images/harry-potter-and-cos.jpg') }}" class="card-img-top img-responsive" alt="Harry Potter Book Poster">
-        <div class="card-body">
-          <h5 class="card-title">Product Title</h5>
-          <p class="card-text description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem corrupti expedita architecto sapiente error quibusdam illum facilis temporibus. Sit, expedita. Dicta excepturi earum et accusamus, quaerat tempora commodi unde itaque.</p>
-          <div class="pull-left price">$12</div>
-          <div class="add-to-cart-container">
-            <a href="#" class="btn btn-success pull-right">Add to Cart</a>
-          </div>
-        </div> 
-      </div>
-    </div>
-    <div class="col-md-4 col-sm-6">
-      <div class="card" style="width: 18rem;">
-        <img src="{{ URL::to('images/harry-potter-and-cos.jpg') }}" class="card-img-top img-responsive" alt="Harry Potter Book Poster">
-        <div class="card-body">
-          <h5 class="card-title">Product Title</h5>
-          <p class="card-text description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem corrupti expedita architecto sapiente error quibusdam illum facilis temporibus. Sit, expedita. Dicta excepturi earum et accusamus, quaerat tempora commodi unde itaque.</p>
-          <div class="pull-left price">$12</div>
-          <div class="add-to-cart-container">
-            <a href="#" class="btn btn-success pull-right">Add to Cart</a>
-          </div>
-        </div> 
-      </div>
-    </div>
-    <div class="col-md-4 col-sm-6">
-      <div class="card" style="width: 18rem;">
-        <img src="{{ URL::to('images/harry-potter-and-cos.jpg') }}" class="card-img-top img-responsive" alt="Harry Potter Book Poster">
-        <div class="card-body">
-          <h5 class="card-title">Product Title</h5>
-          <p class="card-text description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem corrupti expedita architecto sapiente error quibusdam illum facilis temporibus. Sit, expedita. Dicta excepturi earum et accusamus, quaerat tempora commodi unde itaque.</p>
-          <div class="pull-left price">$12</div>
-          <div class="add-to-cart-container">
-            <a href="#" class="btn btn-success pull-right">Add to Cart</a>
-          </div>
-        </div> 
-      </div>
-    </div>
-    <div class="col-md-4 col-sm-6">
-      <div class="card" style="width: 18rem;">
-        <img src="{{ URL::to('images/harry-potter-and-cos.jpg') }}" class="card-img-top img-responsive" alt="Harry Potter Book Poster">
-        <div class="card-body">
-          <h5 class="card-title">Product Title</h5>
-          <p class="card-text description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem corrupti expedita architecto sapiente error quibusdam illum facilis temporibus. Sit, expedita. Dicta excepturi earum et accusamus, quaerat tempora commodi unde itaque.</p>
-          <div class="pull-left price">$12</div>
-          <div class="add-to-cart-container">
-            <a href="#" class="btn btn-success pull-right">Add to Cart</a>
-          </div>
-        </div> 
-      </div>
-    </div>
-  </div>
+    @endforeach
+   </div>
+  @endforeach
 @endsection

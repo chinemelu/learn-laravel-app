54202a4c2d429db2b7c9b835fd1163e38c680ca6
Chinemelu Nwosu <chinemelunwosu@gmail.com>
2021-02-26 23:06:52 +0100

    create shopping cart view

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Brand</a>
+    <a class="navbar-brand" href="{{ route('product.index')}}">Brand</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
